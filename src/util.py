acb5a6dc28519895c1b5cbf26e6c46f919a4ac0a
nilesh05apr <nk8588338@gmail.com>
2022-10-22 01:03:54 +0530

    initial commit

--- src/util.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb+srv://admin:admin@scouto.bcfipnx.mongodb.net/?retryWrites=true&w=majority")
-db = client.BOOKS
-books_db = db.BOOKS_DB
-transaction_db = db.TRANSACTIONS_DB
